3a0ddf838af00f0aee12efacf7eb913af47eef4b
nopeless <38830903+nopeless@users.noreply.github.com>
2026-03-16 14:37:27 -0500

    give a fair chance to naive

--- src/javascript/naive.ts
+++ src/javascript/naive.ts
@@ -9,6 +9,16 @@ export async function processAllCollidingPairs(engine: Engine, callback: (p1: Pa
   // Iterates over $n(n-1)/2$ combinations to mitigate symmetric and self-evaluations.
   for (let i = 0; i < particles.length; i++) {
     for (let j = i + 1; j < particles.length; j++) {
+      // check AABB first
+      const p1 = particles[i];
+      const p2 = particles[j];
+      if (p1.pos.x + p1.radius < p2.pos.x - p2.radius ||
+          p1.pos.x - p1.radius > p2.pos.x + p2.radius ||
+          p1.pos.y + p1.radius < p2.pos.y - p2.radius ||
+          p1.pos.y - p1.radius > p2.pos.y + p2.radius) {
+        continue;
+      }
+
       await callback(particles[i], particles[j]);
     }
   }
