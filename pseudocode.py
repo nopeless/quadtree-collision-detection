dc81952d3d5a749ec5c8df1ed479c5281e661de0
nopeless <38830903+nopeless@users.noreply.github.com>
2026-03-16 22:54:27 -0500

    add pseudocode

---
+++ pseudocode.py
@@ -0,0 +1,44 @@
+class Point:
+    def __init__(self, x, y, data):
+        self.x = x
+        self.y = y
+        self.data = data  # The 'particle' or 'robot'
+
+
+class QuadTree:
+    def __init__(self, boundary, capacity):
+        self.boundary = boundary  # A Rectangle (x, y, w, h)
+        self.capacity = capacity  # Max points before splitting
+        self.points = []  # Points stored in this node
+        self.divided = False  # Tracks if it has children
+
+        # Child pointers (The 4 Quadrants)
+        self.northwest = None
+        self.northeast = None
+        self.southwest = None
+        self.southeast = None
+
+    def subdivide(self):
+        """
+        1. Calculate new boundaries (half width/height)
+        2. Create 4 new QuadTree instances
+        3. Set self.divided = True
+        """
+        pass
+
+    def insert(self, point):
+        """
+        1. If point not in boundary, return False
+        2. If len(points) < capacity, add point and return True
+        3. Otherwise, subdivide() and insert into children leading to return True
+        """
+        pass
+
+    def query(self, rect, found):
+        """
+        1. If boundary doesn't intersect, return found array
+        2. If divided, query deeper quadtrees return found array
+        3. Iterate over self.points and push in found array if rect contains the point
+        4. return found array
+        """
+        pass
